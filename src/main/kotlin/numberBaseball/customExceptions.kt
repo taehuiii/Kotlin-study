2ddd37c76575547c8ca428fc75c6c7490a7b8a28
taehuiii <swanna18@naver.com>
2024-05-01 21:48:46 +0900

    [ADD] fun examine

---
+++ src/main/kotlin/numberBaseball/customExceptions.kt
@@ -0,0 +1,8 @@
+package numberBaseball
+
+import java.lang.Exception
+
+class TooShortInputException(message:String): Exception(message)
+class ZeroInValidInputException(message:String): Exception(message)
+class SameNumberException(message:String): Exception(message)
+
